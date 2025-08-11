a54b057798f728df86dbf63094452af72a04348b
Cleyton Jesus <cleytonjesus007@gmail.com>
2025-08-11 19:25:08 -0300

    refactor: add jotai and change titles on login page

--- frontend/src/components/Layout/DropdownMenu/DropdownMenuNav/SwitchTheme/index.jsx
+++ frontend/src/components/Layout/DropdownMenu/DropdownMenuNav/SwitchTheme/index.jsx
@@ -1,22 +1,26 @@
 import { Switch } from '@headlessui/react';
-import { useTheme } from '../../../../../context/ThemeContext';
+import { useAtomValue, useSetAtom } from 'jotai';
+import {
+  enabledAtom,
+  toggleThemeAtom,
+} from '../../../../../context/atoms/themeApp.atoms';
 
 export default function SwitchTheme() {
-  const { theme, toggleTheme } = useTheme();
-  const enabled = theme === 'dark';
+  const enabled = useAtomValue(enabledAtom);
+  const toggleTheme = useSetAtom(toggleThemeAtom);
 
   return (
     <Switch
       checked={enabled}
       onChange={toggleTheme}
-      className={`group cursor-pointer relative inline-flex h-8 w-16 items-center rounded-full ${
+      className={`group relative inline-flex h-8 w-16 items-center rounded-full ${
         enabled ? 'bg-zinc-600' : 'bg-zinc-200'
       } transition-colors duration-300 focus:outline-none`}
       aria-label="Alternar tema escuro/claro"
     >
       <span
-        className={`size-6 transform rounded-full bg-white shadow-md transition-transform duration-300 translate-x-1 ${
-          enabled ? 'translate-x-9' : ''
+        className={`size-6 transform rounded-full bg-white shadow-md transition-transform duration-300 ${
+          enabled ? 'translate-x-9' : 'translate-x-1'
         }`}
       />
     </Switch>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { routes } from './routes/routes.jsx';
-import { ThemeProvider } from './context/ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={routes} />
-    </ThemeProvider>
+    <RouterProvider router={routes} />
   </StrictMode>
 );
